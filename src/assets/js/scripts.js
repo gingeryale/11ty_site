67c1f2965ec755591cc67c3361e6a9a9eac7d2cf
ao <aolin@ymail.com>
2020-05-15 01:41:43 +0300

    fade-in effect

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,13 +1,28 @@
 console.log("READ dev dev dev dev dev dev dev dev dev dev dev !!!!!!!!!!!!!!");
 
-const hero = document.querySelector('.listing');
+const list = document.querySelector('.listing');
+const faders = document.querySelectorAll('.fade-in');
 
 
-const tl = new TimelineMax();
+const options = {
+    root: null, // viewport
+    threshold: 1, // how much is not the page
+    rootMargin: "0px 0px -50px 0px" // works like margin
+};
+const appearOnScroll = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return
+        }
+        console.log(entry.target);
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+    });
+}, options);
 
-tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-    .fromTo(logo, 0.2, { opacity: 0, x: "-5%" }, { opacity: 1, x: "0%", ease: Power2.easeInOut }, "-=0.7")
-    .fromTo(menu, 0.2, { opacity: 0, x: "55%" }, { opacity: 1, x: "0%", ease: Power2.easeInOut }, "-=0.7")
-    .fromTo(hero, 1.2, { width: '100%' }, { width: "80%", ease: Power2.easeInOut })
-    .fromTo(slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut }, "-=1")
-    .fromTo(headline, 1.2, { x: "-130%" }, { x: "-20%", ease: Power2.easeInOut }, "-=1.2")
+faders.forEach(fade => {
+    appearOnScroll.observe(fade);
+});
+
+appearOnScroll.observe(list);
+
