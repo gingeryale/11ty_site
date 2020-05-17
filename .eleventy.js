b5cbef20771602c249af65bff84a7bc29eb4b5a4
ao <aolin@ymail.com>
2020-05-17 22:40:04 +0300

    svg images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
+const svgContents = require("eleventy-plugin-svg-contents");
+
 module.exports = function (eleventyConfig) {
   // copy files
   eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addPlugin(svgContents);
+
 
   // override default config
   return {
@@ -12,4 +16,8 @@ module.exports = function (eleventyConfig) {
     templateFormats: ["html", "liquid", "njk", "md"],
     markdownTemplateEngine: "njk"
   };
+
+
+
+
 };
