88b2ee1931c0da7518a8cae275e092eb583de6cc
ao <aolin@ymail.com>
2020-08-29 22:14:22 +0300

    removed js fades modified font on skills

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -1,28 +1,28 @@
-const article = document.querySelector('article');
-const faders = document.querySelectorAll('.fade-in');
+// const article = document.querySelector('article');
+// const faders = document.querySelectorAll('.fade-in');
 
 
-const options = {
-    root: null, // viewport
-    threshold: 1, // how much is not the page
-    rootMargin: "0px 0px 240px 0px" // works like margin
-};
-const appearOnScroll = new IntersectionObserver(function (entries, options) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return
-        }
-        console.log(entry.target);
-        entry.target.classList.add('appear');
-        appearOnScroll.unobserve(entry.target);
-    });
-}, options);
+// const options = {
+//     root: null, // viewport
+//     threshold: 1, // how much is not the page
+//     rootMargin: "0px 0px 240px 0px" // works like margin
+// };
+// const appearOnScroll = new IntersectionObserver(function (entries, options) {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//             return
+//         }
+//         console.log(entry.target);
+//         entry.target.classList.add('appear');
+//         appearOnScroll.unobserve(entry.target);
+//     });
+// }, options);
 
-faders.forEach(fade => {
-    appearOnScroll.observe(fade);
-});
+// faders.forEach(fade => {
+//     appearOnScroll.observe(fade);
+// });
 
-appearOnScroll.observe(article);
+// appearOnScroll.observe(article);
 
 
 
